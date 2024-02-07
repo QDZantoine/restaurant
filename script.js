4fdc488b2be40d5b0a72efe20cbd8570844ed5be
QDZantoine <antoine.quendez@gmail.com>
2024-02-07 14:21:22 +0100

    💄 update css

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+//---------NAVBAR MOBILE-----
+
 const mobileMenuButton = document.getElementById('mobile-menu-button');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 mobileMenuButton.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
 });
+//---------SCROLLING NAVBAR-------
+// Fonction qui ajoute la classe fixed-nav à la barre de navigation lors du défilement
+function handleScroll() {
+    var navbar = document.querySelector('nav'); // Remplacez 'nav' par le sélecteur approprié si nécessaire
+    var scrollDistance = window.scrollY; // Distance défilée depuis le haut de la page
+  
+    // Vous pouvez ajuster '100' à la distance de défilement souhaitée
+    if (scrollDistance > 100) {
+      navbar.classList.add('fixed-nav');
+    } else {
+      navbar.classList.remove('fixed-nav');
+    }
+  }
+  
+  // Écoutez l'événement de défilement sur la fenêtre
+  window.addEventListener('scroll', handleScroll);
+  
 
 //------slider menu -----//
 const menus = document.querySelectorAll('.menu-card');
